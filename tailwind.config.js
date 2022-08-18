70699a8f83c1a644774eb48f8685a82d2f189ec3
Shivam Vijaywargi <vjshivam5@gmail.com>
2022-08-18 23:08:57 +0530

    PayTm clone using TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        lightBlue: '#66d6ff',
+        mediumBlue: '#00baf2',
+        darkBlue: '#002970',
+        lightWhite: '#f5f7fa',
+      },
+    },
+  },
+  plugins: [],
+};
